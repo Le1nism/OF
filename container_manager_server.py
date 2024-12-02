d1c83efc8fd5801176e37a7a970f7e1f35a03ee7
QwertyJacob <jesusfcevallos@gmail.com>
2024-12-02 11:08:05 +0100

    firing producing from server

--- container_manager_server.py
+++ container_manager_server.py
@@ -1,16 +1,14 @@
 import docker
-import threading
-import time
-import subprocess
 import json
-import random
-import yaml
 import http.server
 import socketserver
 import json
 from omegaconf import DictConfig, OmegaConf 
 import hydra
+import threading
 
+
+PRODUCER_COMMAND = "python synthetic_data_generator.py"
 
 def run_command_in_container(container, command):
     # Run the command in the container shell to obtain the PID
@@ -19,22 +17,46 @@ def run_command_in_container(container, command):
     return pid
 
 def refresh_containers():
-    global containers_dict, containers_ips
+    global containers_dict, containers_ips, producers, consumers
     # Connect to the Docker daemon
     client = docker.from_env()
 
     for container in client.containers.list():
-
         container_info = client.api.inspect_container(container.id)
         # Extract the IP address of the container from its network settings
         container_info_str = container_info['Config']['Hostname']
         container_img_name = container_info_str.split('(')[0]
         container_ip = container_info['NetworkSettings']['Networks']['trainsensordatavisualization_trains_network']['IPAddress']
         print(f'{container_img_name} is {container.name} with ip {container_ip}')
+        if 'producer' in container_img_name:
+            producers[container_img_name] = container
+        elif 'consumer' in container_img_name:
+            consumers[container_img_name] = container
         containers_dict[container_img_name] = container
         containers_ips[container_img_name] = container_ip 
     print('\n\n\n')
             
+
+def print_output(container, command, thread_name):
+    # Execute the command in the container and stream the output
+    return_tuple = container.exec_run(command, stream=True, tty=True, stdin=True)
+    for line in return_tuple[1]:
+        print(thread_name+": "+line.decode().strip())  # Print the output line by line
+
+
+
+def produce_all():
+    global producers
+    for producer_name, producer_container in producers.items():
+        output_thread = threading.Thread(
+            target=print_output, 
+            args=(producer_container, PRODUCER_COMMAND, producer_name))
+        output_thread.start()
+        print(f"Started producing from {producer_name}")
+        
+    return "All producers started!"
+
+
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
@@ -45,6 +67,16 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.end_headers()
                 response = {'message': 'Containers refreshed!'}
                 self.wfile.write(json.dumps(response).encode())
+            elif self.path == '/produce_all':
+                response_str = produce_all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')  
+                self.end_headers()
+                self.wfile.write(response_str.encode()) 
+            elif self.path == '/stop_producing_all':
+                stop_producing_all()
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
             else:
                 super().do_GET()
 
@@ -56,7 +88,7 @@ class ReusableTCPServer(socketserver.TCPServer):
 
 @hydra.main(config_path="config", config_name="default", version_base="1.2")
 def main(cfg: DictConfig) -> None:
-    global containers_dict, containers_ips
+    global containers_dict, containers_ips, consumers, producers
     print("\n________________________________________________________________\n\n"+\
           "               OPEN FAIR Container Manager \n" +\
           "________________________________________________________________\n"+\
@@ -65,6 +97,8 @@ def main(cfg: DictConfig) -> None:
           "            - Re-launch this script each time you change container status (through node restart). \n\n\n")
     
     containers_dict = {}
+    consumers = {}
+    producers = {}
     containers_ips = {}
     refresh_containers() 
     
