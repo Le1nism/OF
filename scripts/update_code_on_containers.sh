5640b63c1eb3b6e8ae931d2c9a8300432ade62e9
QwertyJacob <jesusfcevallos@gmail.com>
2025-02-01 22:53:21 +0100

    refac

--- scripts/update_code_on_containers.sh
+++ scripts/update_code_on_containers.sh
@@ -5,5 +5,5 @@ containers=("claude_consumer" "claude_producer" "angela_consumer" "angela_produc
 # Loop through each container and perform git pull
 for container in "${containers[@]}"; do
     echo "Updating code in $container..."
-    docker exec -it $container /bin/bash -c 'git pull'
+    docker exec -it $container /bin/bash -c 'git pull --rebase'
 done
