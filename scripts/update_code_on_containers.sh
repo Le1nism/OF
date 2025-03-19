53eae15321e8a95a7d4d5303da347dca12a83225
QwertyJacob <jesusfcevallos@gmail.com>
2025-03-19 11:04:43 +0100

    update

--- scripts/update_code_on_containers.sh
+++ scripts/update_code_on_containers.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # List of container names
-containers=("claude_consumer" "claude_producer" "angela_consumer" "angela_producer" "bob_consumer" "bob_producer" "wandber")
+containers=("claude_consumer" \
+            "claude_producer" \
+            "angela_consumer" \
+            "angela_producer" \
+            "bob_consumer" \
+            "bob_producer" \
+            "frank_consumer" \
+            "frank_producer" \
+            "emily_consumer" \
+            "emily_producer" \
+            "daniel_consumer" \
+            "daniel_producer" \
+            "wandber")
 
 # Loop through each container and perform git pull
 for container in "${containers[@]}"; do
